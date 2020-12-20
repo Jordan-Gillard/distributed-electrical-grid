3af8ee9f13ed33f3a30c0068fa15719bc67855a9
Jordan Gillard <jordan-gillard@outlook.com>
2020-12-20 13:21:56 -0600

    bugfix: Fixes all build issues with Maven

--- src/test/java/energy_resources/EnergyresourcesApplicationTests.java
+++ src/test/java/energy_resources/EnergyresourcesApplicationTests.java
@@ -1,7 +1,7 @@
-package test.java;
+package energy_resources;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.testng.annotations.Test;
 
 @SpringBootTest
 class EnergyresourcesApplicationTests {
