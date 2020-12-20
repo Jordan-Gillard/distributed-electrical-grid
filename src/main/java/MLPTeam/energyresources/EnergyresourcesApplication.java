e36bb557432163538b8a7592d6e8fbab812b8412
Jordan Gillard <jordan-gillard@outlook.com>
2020-12-20 09:22:04 -0600

    Initial commit with Spring Boot generated project

---
+++ src/main/java/MLPTeam/energyresources/EnergyresourcesApplication.java
@@ -0,0 +1,13 @@
+package MLPTeam.energyresources;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnergyresourcesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnergyresourcesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/MLPTeam/energyresources/EnergyresourcesApplicationTests.java
@@ -0,0 +1,13 @@
+package MLPTeam.energyresources;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnergyresourcesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
