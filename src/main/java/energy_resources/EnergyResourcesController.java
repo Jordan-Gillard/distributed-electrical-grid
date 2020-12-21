c13d9a40bcb3e14db85dbbe9eb57f470883d29ea
Jordan Gillard <jordan-gillard@outlook.com>
2020-12-21 07:21:41 -0600

    docs: Adds updated system diagram to project README

--- src/main/java/energy_resources/EnergyResourcesController.java
+++ src/main/java/energy_resources/EnergyResourcesController.java
@@ -1,13 +1,13 @@
 package energy_resources;
 
 import avro.BatteryEvent;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class EnergyResourcesController {
 
-    @GetMapping("/event")
+    @PostMapping("/event")
     public void handleEvent(BatteryEvent batteryEvent) {
         System.out.println(batteryEvent);
     }
