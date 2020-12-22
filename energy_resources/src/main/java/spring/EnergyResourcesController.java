1691329360ca70bfc3535d2e25f3cc4f3200abf3
ou <xu16@vt.edu>
2020-12-22 10:54:04 -0500

    refactor: Add a new time field to the Avro BatteryEvent schema.

--- energy_resources/src/main/java/spring/EnergyResourcesController.java
+++ energy_resources/src/main/java/spring/EnergyResourcesController.java
@@ -5,12 +5,17 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Date;
+
 @RestController
 public class EnergyResourcesController {
+    private final Date date = new Date();
+
     @PostMapping("/event/{uuid}")
     public void handlePostEvent(
         @PathVariable("uuid") String uuid,
         BatteryEvent batteryEvent) {
+        batteryEvent.setTime(date.getTime());
         System.out.println(batteryEvent);
     }
 }
