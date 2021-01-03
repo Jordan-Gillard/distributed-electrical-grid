5275331a21db79fde89e461fe9f7a5231046fc80
Jordan Gillard <jordan-gillard@outlook.com>
2021-01-03 14:44:52 -0600

    refactor: Renames variables and cleans code in Database class

--- energy_resources/src/main/java/database/Database.java
+++ energy_resources/src/main/java/database/Database.java
@@ -10,25 +10,29 @@ public class Database {
     static final String PASS = "example1";
 
 
-    public static void main(String[] args) {
+    public static void createEmptyBatteryEventTable() {
         Jdbi jdbi = getJdbiConnectorWithDataSource();
-        String dropTable = "DROP TABLE IF EXISTS batteryEvent";
-        String sql =
-            "CREATE TABLE IF NOT EXISTS batteryEvent " + "(batteryEventID INTEGER AUTO_INCREMENT," + "charging_source VARCHAR(255)," + "processor4_temp int," + "device_id VARCHAR(255)," + "processor2_temp int," + "processor1_temp int," + "charging int," + "current_capacity int," + "inverter_state int," + "moduleL_temp int," + "moduleR_temp int," + "processor3_temp int," + "soC_regulator float," + "time timestamp," + "PRIMARY KEY (batteryEventID))";
+        String dropBatteryEventTable = "DROP TABLE IF EXISTS batteryEvent";
+        String createBatteryEventTable =
+            "CREATE TABLE IF NOT EXISTS batteryEvent "
+                + "(batteryEventID INTEGER AUTO_INCREMENT,"
+                + "charging_source VARCHAR(255)," + "processor4_temp int,"
+                + "device_id VARCHAR(255)," + "processor2_temp int,"
+                + "processor1_temp int," + "charging int,"
+                + "current_capacity int," + "inverter_state int,"
+                + "moduleL_temp int," + "moduleR_temp int,"
+                + "processor3_temp int," + "soC_regulator float,"
+                + "time timestamp," + "PRIMARY KEY (batteryEventID))";
         jdbi.useHandle(handle -> {
-            handle.execute(dropTable);
-            handle.execute(sql);
-            handle.execute(
-                "INSERT INTO electricalGridDB.batteryEvent(charging_source, charging) VALUES (?, ?)",
-                "beach", 11);
+            handle.execute(dropBatteryEventTable);
+            handle.execute(createBatteryEventTable);
         });
     }
 
 
     public static Jdbi getJdbiConnectorWithDataSource() {
         BasicDataSource dataSource = getBasicDataSource();
-        Jdbi jdbi = Jdbi.create(dataSource);
-        return jdbi;
+        return Jdbi.create(dataSource);
     }
 
 
@@ -41,5 +45,4 @@ public class Database {
         dataSource.setValidationQuery("SELECT 1");
         return dataSource;
     }
-
 }
